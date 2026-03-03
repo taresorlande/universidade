3f3f50ee855b0d91a0df6c072a2d3d0abeb6bc16
Tares Liberato Orlande de Almeida <13944@senacminas.edu.br>
2026-03-03 14:56:40 -0300

    tela inicial de funcionalidades

--- python/app.py
+++ python/app.py
@@ -1,15 +1,47 @@
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import (
+    QApplication,
+    QVBoxLayout,
+    QPushButton,
+    QWidget
+)
 
 from screen.cadastrar import Cadastrar
 from screen.listar import Listar
 
 import sys
 
+class App:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+
+        self.janela = QWidget()
+        self.layout = QVBoxLayout()
+
+        self.janela.setWindowTitle("Sistema Universidade")
+        self.janela.resize(400, 200)
+        self.janela.setLayout(self.layout)
+
+        self.criar_botoes()
+
+        self.janela.show()
+    
+    def criar_botoes(self):
+        botao_listar = QPushButton("Listar")
+        self.layout.addWidget(botao_listar)
+        botao_listar.clicked.connect(self.abrir_listagem)
+
+        botao_cadastrar= QPushButton("Cadastrar")
+        self.layout.addWidget(botao_cadastrar)
+        botao_cadastrar.clicked.connect(self.abrir_cadastro)
+
+    def abrir_listagem(self):
+        self.tela_listagem = Listar(self.app)
+        self.tela_listagem.janela.show()
+    
+    def abrir_cadastro(self):
+        self.tela_cadastro = Cadastrar(self.app)
+        self.tela_cadastro.janela.show()
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    # tela = Cadastrar(app)
-    tela = Listar(app)
-    tela.janela.show()
-
-    sys.exit(tela.app.exec())
+    system = App()
+    sys.exit(system.app.exec())
